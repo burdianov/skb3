3c52a7a14ffd41499c9bfefeaa41d4c922bfe30f
Lilian Burdianov <lilian.burdianov@gmail.com>
2016-11-09 15:53:05 +0400

    MongoDB manipulation

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+import bodyParser from 'body-parser';
+import saveDataInDb from './saveDataInDb';
+import Pet from './models/Pet';
+import User from './models/User';
+import isAdmin from './middlewares/isAdmin';
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://publicdb.mgbeta.ru/lburdianov_skb3');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+// app.use(isAdmin);
+
+app.get('/clear', isAdmin, async (req, res) => {
+  await User.remove({});
+  await Pet.remove({});
+  return res.send('OK');
+});
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  return res.json(users);
+});
+
+app.get('/pets', async (req, res) => {
+  const pets = await Pet.find().populate('owner');
+  return res.json(pets);
+});
+
+app.post('/data', async (req, res) => {
+  const data = req.body;
+  if (!data.user) return res.status(400).send('user required');
+  if (!data.pets) data.pets = [];
+
+  const user = await User.findOne({
+    name: data.user.name,
+  });
+  if (user) return res.status(400).send('user.name exists');
+  try {
+    const result = await saveDataInDb(data);
+    return res.json(result);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
+
+// const data = {
+//   user: {
+//     name: 'lburdianov',
+//   },
+//   pets: [
+//     {
+//       name: 'Max',
+//       type: 'cat',
+//     },
+//     {
+//       name: 'Pincher',
+//       type: 'dog',
+//     },
+//   ],
+// };
+
+/////////////// = first comment ///////////////
+
+// const Pet = mongoose.model('Pet', {
+//   type: String,
+//   name: String,
+// });
+
+// const kitty = new Pet({
+//   type: 'cat',
+//   name: 'Zildjian',
+// });
+
+// kitty.save()
+// .then(() => {
+//   console.log('success');
+// })
+// .catch((err) => {
+//   console.log('err', err);
+// });
+
+// const app = express();
+// app.use(cors());
+
+//////////////// first comment end ////////////////
